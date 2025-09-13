cad0bede7c7ded23bff4f41ea646e08b27795c7d
saprykins <palatatot@gmail.com>
2025-09-13 14:25:21 +0200

    Initial commit of all project files

---
+++ readme.txt
@@ -0,0 +1,2 @@
+working version
+it creates a bot, uses openai api to answer the user
---
+++ src/agent.ts
@@ -0,0 +1,42 @@
+import { ActivityTypes } from "@microsoft/agents-activity";
+import { AgentApplication, MemoryStorage, TurnContext } from "@microsoft/agents-hosting";
+import { AzureOpenAI, OpenAI } from "openai";
+import config from "./config";
+
+const client = new OpenAI({
+  apiKey: config.openAIKey,
+});
+const systemPrompt = "You are an AI agent that can chat with users.";
+
+// Define storage and application
+const storage = new MemoryStorage();
+export const agentApp = new AgentApplication({
+  storage,
+});
+
+agentApp.conversationUpdate("membersAdded", async (context: TurnContext) => {
+  await context.sendActivity(`Hi there! I'm an agent to chat with you.`);
+});
+
+// Listen for ANY message to be received. MUST BE AFTER ANY OTHER MESSAGE HANDLERS
+agentApp.activity(ActivityTypes.Message, async (context: TurnContext) => {
+  // Echo back users request
+  const result = await client.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt,
+      },
+      {
+        role: "user",
+        content: context.activity.text,
+      },
+    ],
+    model: config.openAIModelName
+  });
+  let answer = "";
+  for (const choice of result.choices) {
+    answer += choice.message.content;
+  }
+  await context.sendActivity(answer);
+});
